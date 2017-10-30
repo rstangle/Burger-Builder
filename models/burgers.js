99e86ac1e7630f99fc4c4ae0bb8f34b8e5d16007
Stangle <rstangle@gmail.com>
2017-10-29 22:51:25 -0400

    handlebars layout and CSS complete

---
+++ models/burgers.js
@@ -0,0 +1,16 @@
+var orm = require("../config/orm.js");
+
+// Code to call the ORM functions using burger specific input for the ORM
+//=====================================================================================
+
+// var burger = {
+
+
+// };
+
+//=====================================================================================
+
+// Export the database functions for the controller (catsController.js).
+// module.exports = burgers;
+
+//=====================================================================================
